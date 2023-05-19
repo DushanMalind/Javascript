f8ed562937597c8827ce4bc66d81f153c6c7b5b4
DushanMalind <dushanmalinda010@gmail.com>
2023-05-19 22:10:02 +0530

    action event button

---
+++ .idea/O2_DOM_Manipulation/indextest.js
@@ -0,0 +1,26 @@
+document.getElementById("btn1")
+document.getElementById("btn1").innerText="DD";
+'DD'
+document.getElementById("btn1").innerText="Set Text"
+'Set Text'
+
+document.getElementsByName("txtContent")[0];
+document.getElementsByClassName("btn");
+document.getElementsByTagName('h1');
+document.getElementsByTagName('input');
+document.getElementsByName('');
+document.querySelector("input");
+
+
+document.getElementById("txtOne").value;
+
+document.getElementById("txtOne").value;
+
+document.getElementById("txtOne").value="GDSE";
+
+document.getElementById("btn1").addEventListener("click",function(){
+/*    document.getElementById("txtOne").value;*/
+    document.querySelector('.display').innerText=document.getElementById("txtOne").value;
+    alert("Hello");
+});
+
